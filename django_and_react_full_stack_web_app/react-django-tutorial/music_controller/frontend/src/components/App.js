b945fdf3b1a8df7751c1926109afeb6b47d803d7
philipstubbs13 <philipstubbs13@gmail.com>
2021-01-12 05:51:16 -0600

    react router and building components

--- django_and_react_full_stack_web_app/react-django-tutorial/music_controller/frontend/src/components/App.js
+++ django_and_react_full_stack_web_app/react-django-tutorial/music_controller/frontend/src/components/App.js
@@ -1,6 +1,7 @@
 // @ts-nocheck
 import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from './HomePage';
 
 export default class App extends Component {
   constructor(props) {
@@ -8,7 +9,11 @@ export default class App extends Component {
   }
 
   render() {
-    return <h1>Testing React Code</h1>;
+    return (
+      <div>
+        <HomePage />
+      </div>
+    )
   }
 }
 
